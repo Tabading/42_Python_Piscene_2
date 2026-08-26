2b9698da12f48018e13c8e1029c736c436143997
Tabading <tarlaxo@gmail.com>
2026-08-26 15:19:25 +0200

    ex00 changed to include return null

--- ex00/load_csv.py
+++ ex00/load_csv.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 def load(path: str) -> pd.DataFrame:
     assert type(path) is str, "path not string."
-    df = pd.read_csv(path)
-    print("Loading dataset of dimensions", df.shape)
-    return df
+    try:
+        df = pd.read_csv(path)
+        print("Loading dataset of dimensions", df.shape)
+        return df
+    except Exception:
+        pass
+    return None
